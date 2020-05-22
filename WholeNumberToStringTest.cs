f46d06b7c70857251299f2b9e5d686ff634b3578
Shwetha-Krishnarao <64847319+Shwetha-Krishnarao@users.noreply.github.com>
2020-05-22 13:47:01 +0530

    Create WholeNumberToStringTest.cs

---
+++ WholeNumberToStringTest.cs
@@ -0,0 +1,53 @@
+using NUnit.Framework;
+namespace NUnitUnitTests
+{
+    // A class that contains NUnit unit tests
+    [TestFixture]
+    public class WholeNumberToStringTest
+    {  
+        public void TestInit()
+        {
+         //Initialization of data goes here   
+        }
+        [Test]
+        public void TestConvertToWords()
+        {
+        //Assume
+        
+        //Act
+        
+        //Assert
+        }
+        
+        [Test]
+        public void TestConvertDecimals()
+        {
+         //Assume
+        
+        //Act
+        
+        //Assert
+        }
+        
+         [Test]
+        public void TestConvertToWords()
+        {
+         //Assume
+        
+        //Act
+        
+        //Assert
+        }
+       
+        public void TestCleanup()
+        {
+            // Runs after each test. (Optional)
+        }
+       
+        public void ClassCleanup()
+        {
+            // Runs once after all tests in this class are executed. (Optional)
+            // Not guaranteed that it executes instantly after all tests from the class.
+        }
+    }
+}
