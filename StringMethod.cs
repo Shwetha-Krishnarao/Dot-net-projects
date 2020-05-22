e728cfdb4fd3485fc6cd5448d592ecdbbace5a9d
Shwetha-Krishnarao <64847319+Shwetha-Krishnarao@users.noreply.github.com>
2020-05-22 13:21:01 +0530

    Update StringMethod.cs

--- StringMethod.cs
+++ StringMethod.cs
@@ -1,3 +1,5 @@
+/* Function to Convert string from 1 to 10 */
+
 private static String ones(String Number)  
 {  
     int _Number = Convert.ToInt32(Number);  
@@ -31,6 +33,77 @@ private static String ones(String Number)
             break;  
         case 9:  
             name = "Nine";  
+            break;  
+    }  
+    return name;  
+}
+
+/* Function to Convert string from 10 to 10o */
+private static String tens(String Number)  
+{  
+    int _Number = Convert.ToInt32(Number);  
+    String name = null;  
+    switch (_Number)  
+    {  
+        case 10:  
+            name = "Ten";  
+            break;  
+        case 11:  
+            name = "Eleven";  
+            break;  
+        case 12:  
+            name = "Twelve";  
+            break;  
+        case 13:  
+            name = "Thirteen";  
+            break;  
+        case 14:  
+            name = "Fourteen";  
+            break;  
+        case 15:  
+            name = "Fifteen";  
+            break;  
+        case 16:  
+            name = "Sixteen";  
+            break;  
+        case 17:  
+            name = "Seventeen";  
+            break;  
+        case 18:  
+            name = "Eighteen";  
+            break;  
+        case 19:  
+            name = "Nineteen";  
+            break;  
+        case 20:  
+            name = "Twenty";  
+            break;  
+        case 30:  
+            name = "Thirty";  
+            break;  
+        case 40:  
+            name = "Fourty";  
+            break;  
+        case 50:  
+            name = "Fifty";  
+            break;  
+        case 60:  
+            name = "Sixty";  
+            break;  
+        case 70:  
+            name = "Seventy";  
+            break;  
+        case 80:  
+            name = "Eighty";  
+            break;  
+        case 90:  
+            name = "Ninety";  
+            break;  
+        default:  
+            if (_Number > 0)  
+            {  
+                name = tens(Number.Substring(0, 1) + "0") + " " + ones(Number.Substring(1));  
+            }  
             break;  
     }  
     return name;  
